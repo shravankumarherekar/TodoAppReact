54e4590a732e38d9541129616b0c465541a607a4
shravankumarherkar <shravan.herekar01@gmail.com>
2023-12-23 19:12:06 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,186 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-function App() {
+  useEffect(() => {
+    // Fetching Todo List
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const data = await response.json();
+        setTodos(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const addTodo = () => {
+    if (newTodo.trim() !== "") {
+      const newTodoItem = {
+        userId: 1,
+        id: todos.length + 1,
+        title: newTodo,
+        completed: false,
+      };
+
+      setTodos([...todos, newTodoItem]);
+      setNewTodo("");
+    }
+  };
+
+  const editTodo = (id, editedTitle) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, title: editedTitle } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const handleSearchChange = (event) => {
+    const term = event.target.value;
+    setSearchTerm(term);
+    const filteredSuggestions = todos.filter((todo) =>
+      todo.title.toLowerCase().includes(term.toLowerCase())
+    );
+    setSuggestions(filteredSuggestions);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>
+    //   <h1>Todo App</h1>
+
+    //   {/* Add Todo */}
+    //   <div>
+    //     <input
+    //       type="text"
+    //       placeholder="Add a new todo"
+    //       value={newTodo}
+    //       onChange={(e) => setNewTodo(e.target.value)}
+    //     />
+    //     <button onClick={addTodo}>Add Todo</button>
+    //   </div>
+
+    //   {/* Todo List */}
+    //   <div>
+    //     <h2>Todo List</h2>
+    //     {loading ? (
+    //       <p>Loading...</p>
+    //     ) : (
+    //       <ul>
+    //         {todos.map((todo) => (
+    //           <li key={todo.id}>
+    //             <span>{todo.title}</span>
+    //             <button
+    //               onClick={() =>
+    //                 editTodo(todo.id, prompt("Enter new title:", todo.title))
+    //               }
+    //             >
+    //               Edit
+    //             </button>
+    //             <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     )}
+    //   </div>
+
+    //   {/* Search and Suggestions */}
+    //   <div>
+    //     <input
+    //       type="text"
+    //       placeholder="Search todos"
+    //       value={searchTerm}
+    //       onChange={handleSearchChange}
+    //     />
+    //     <ul>
+    //       {suggestions.map((todo) => (
+    //         <li key={todo.id}>{todo.title}</li>
+    //       ))}
+    //     </ul>
+    //   </div>
+    // </div>
+    <div className="app">
+      <h1 className="header">Todo App</h1>
+
+      {/* Add Todo */}
+      <div className="add-todo">
+        <input
+          type="text"
+          className="input"
+          placeholder="Add a new todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button className="button" onClick={addTodo}>
+          Add Todo
+        </button>
+      </div>
+
+      {/* Todo List */}
+      <div className="todo-list">
+        <h2 className="list-header">Todo List</h2>
+        {loading ? (
+          <p className="loading">Loading...</p>
+        ) : (
+          <ul className="todo-items">
+            {todos.map((todo) => (
+              <li key={todo.id} className="todo-item">
+                <span className="todo-title">{todo.title}</span>
+                <button
+                  className="edit-button"
+                  onClick={() =>
+                    editTodo(todo.id, prompt("Enter new title:", todo.title))
+                  }
+                >
+                  Edit
+                </button>
+                <button
+                  className="delete-button"
+                  onClick={() => deleteTodo(todo.id)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      {/* Search and Suggestions */}
+      <div>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search todos"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <ul className="suggestions-list">
+          {suggestions.map((todo) => (
+            <li key={todo.id} className="suggestion">
+              {todo.title}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
